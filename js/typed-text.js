65f1b649350eb97a3612e0ac03aa690820044b1a
Ikaterin-crtl <catarinaneedsajob@gmail.com>
2025-06-11 15:42:15 -0300

    commit finalização da tela inicial (boas-vindas)

---
+++ js/typed-text.js
@@ -0,0 +1,23 @@
+const phrases = [
+    "Transforme seu aprendizado.",
+    "Conteúdo adaptado para você.",
+    "MindED torna estudar mais leve.",
+    "Seu estudo. Seu ritmo. Sua forma."
+];
+
+let currentPhrase = 0;
+let typedText = document.getElementById("typed-text");
+
+function showPhrase() {
+    typedText.classList.remove("fade-in");
+    void typedText.offsetWidth; 
+    typedText.textContent = phrases[currentPhrase];
+    typedText.classList.add("fade-in");
+
+    currentPhrase = (currentPhrase + 1) % phrases.length;
+}
+
+setInterval(showPhrase, 3000);
+
+
+typedText.classList.add("fade-in");
