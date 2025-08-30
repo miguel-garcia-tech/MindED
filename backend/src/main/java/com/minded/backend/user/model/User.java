1418d358ecdca4678376dfe7e0434b1faad125ca
migu3 <miguel.garcia.santos16@gmail.com>
2025-08-29 21:42:28 -0300

    Revert "classes iniciais e login"
    This reverts commit 0846412b52d65f05dedbc7813bd9585e7d2c6f17.

--- backend/src/main/java/com/minded/backend/user/model/User.java
+++ backend/src/main/java/com/minded/backend/user/model/User.java
@@ -1,14 +1,6 @@
 package com.minded.backend.user.model;
 
-@Entity
-@Table(name = "users") // nome da tabela no banco
 public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    private String nomeCompleto;
-    private String email;
     private String username;
     private String password; // In a real app, this should be hashed
 
@@ -27,29 +19,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNomeCompleto() {
-        return nomeCompleto;
-    }
-
-    public void setNomeCompleto(String nomeCompleto) {
-        this.nomeCompleto = nomeCompleto;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
